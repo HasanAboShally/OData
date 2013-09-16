baa122995cabf0419005ba765436d7ae883bf846
hakam-k <hakam.kittany@gmail.com>
2013-09-16 11:30:55 +0300

    submit intellisense

---
+++ temp/intellisense/intellisense.js
@@ -0,0 +1,27 @@
+﻿
+(function () {
+
+    function funcDir() {
+        return {
+            restrict: 'E',
+            templateUrl: 'directives/intellisense/intellisense-template.html',
+            scope: {
+
+                topY: '@',
+                leftX: '@',
+                sourceData: '=', // to pass array must be two way binding
+                setSelectedItem: '&'
+            },
+            link: function(scope, iElement, iAttrs, controller) {
+                
+                scope.$watch('topY+leftX', function () {
+                    $('.cus-container').css({ top: scope.topY + 'px', left: scope.leftX + 'px' });
+                });
+               
+
+            }
+        }
+    };
+    angular.module('Intellisense').directive('intellisense', funcDir);
+    
+})();
---
+++ temp/intellisense/toolTip.js
@@ -0,0 +1,32 @@
+﻿(function () {
+
+    function ToolTip() {
+        return {
+            restrict: 'C',
+            link: function (scope, iElement, iAttrs, controller) {
+  
+                var desc = scope.item.description;
+                $(iElement).tooltip({ 'selector': '', 'placement': 'right', 'title': desc, 'trigger': 'hover focus' });
+                if (scope.item.type == 'entity') {
+                    $(iElement).children('i').addClass("entity");
+                }
+                if (scope.item.type == 'query') {
+                    $(iElement).children('i').addClass("icon-usd");
+                }
+
+                $(iElement).click(function () {
+
+                    // list hover and selected item active 
+                    $('.li-list').removeClass("li-active");
+                    $(this).addClass("li-active");
+
+                    // update selected item
+                    scope.$parent.setSelectedItem({item:$(this).children('span').html()});
+                    
+
+                });
+            }
+        }
+    };
+    angular.module('Intellisense').directive('toolTip', ToolTip);
+})();
